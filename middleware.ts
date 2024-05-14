2fbed1c6b8de4ebf9b85c6d35cb267b7e2c74e35
Supreme Khadka <supremekhadka2@gmail.com>
2024-05-14 20:17:34 +0545

    updated middleware.ts and home page

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
 import { clerkMiddleware, ClerkMiddlewareAuth } from "@clerk/nextjs/server";
 
+console.log("NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
